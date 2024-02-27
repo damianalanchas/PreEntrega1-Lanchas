6b8b8e0c9e6281e0ef876ec906fdf52b0747257d
Damiana Lanchas <damiana.lanchas@gmail.com>
2024-02-26 21:11:23 -0300

    primera version

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-// DECLARACIONES
+// OBJETOS / CLASES
 
-const dolarHoy = 879.62;
+class Moneda {
+    constructor(nombre,precio){
+        this.nombre = nombre;
+        this.precio = precio
+    }
+}
 
-const euroHoy = 950.52;
+const dolar = new Moneda ("dolares", 859.50)
+const euro = new Moneda ("euros", 912.20)
+const real = new Moneda ("reales", 168.83)
+const libraEsterlina = new Moneda ("libras esterlinas", 1066.41)
 
-const realHoy = 166.50;
+// ARRAYS
 
-const libraEsterlinaHoy = 1043.40;
-
-const moneda1 = "dólares";
-
-const moneda2 = "euros";
-
-const moneda3 = "reales";
-
-const moneda4 = "libras esterlinas";
+const historial = [];
 
 
 //FUNCION
 
 function conversionMoneda (pesosArgentinos, valorMoneda, moneda){
 
-    alert ("$" + valorEnPesos + " equivalen a " + (pesosArgentinos / valorMoneda) + " " + moneda)
+    alert ("$" + valorEnPesos + " equivalen a " + (pesosArgentinos / valorMoneda).toFixed(2) + " " + moneda)
+    const operacionAGuardar ={
+        valorEnPesos: valorEnPesos,
+        resultado: (pesosArgentinos / valorMoneda).toFixed(2),
+        moneda: moneda
+    }
+    
+    historial.push (operacionAGuardar)
 }
     
 
@@ -37,31 +44,42 @@ let valorEnPesos = prompt ("Ingrese valor en Pesos Argentinos que desea converti
 
 //CONDICIONAL
 
-while (valorEnPesos != "ESC" && valorEnPesos != "esc"){
+while (valorEnPesos.toLowerCase() != "esc" && valorEnPesos.toLowerCase() != "historial"){
 
     let monedaElegida = parseInt (prompt ("Ingrese moneda en la que quiere hacer la conversión: \n 1 - Dólares \n 2 - Euros \n 3 - Reales \n 4 - Libras Esterlinas"));
 
     switch (monedaElegida){
 
         case 1:
-            conversionMoneda (valorEnPesos, dolarHoy, moneda1)
+            conversionMoneda (valorEnPesos, dolar.precio, dolar.nombre)
             break;
 
         case 2:
-            conversionMoneda (valorEnPesos, euroHoy, moneda2)
+            conversionMoneda (valorEnPesos, euro.precio, euro.nombre)
             break;
 
         case 3:
-            conversionMoneda (valorEnPesos, realHoy, moneda3)
+            conversionMoneda (valorEnPesos, real.precio, real.nombre)
             break;
 
         case 4:
-            conversionMoneda (valorEnPesos, libraEsterlinaHoy, moneda4)
+            conversionMoneda (valorEnPesos, libraEsterlina.precio, libraEsterlina.nombre)
             break;
 
         default:
             alert ("Moneda ingresada incorrecta. Volver a intentar.");
     }
 
-    valorEnPesos = prompt ("Ingrese valor en Pesos Argentinos que desea convertir.\n ESC para salir.");
+    valorEnPesos = prompt ("Ingrese valor en Pesos Argentinos que desea convertir o escriba HISTORIAL para ver el mismo.\n ESC para salir.");
+
+    if (valorEnPesos.toLowerCase() === "historial"){
+
+        let mensaje = "Ultimas conversiones realizadas: \n";
+    
+        historial.forEach (operacion => {
+            mensaje = mensaje + `El valor de ${operacion.valorEnPesos} pesos argentinos es equivalente a ${operacion.resultado} ${operacion.moneda}.\n`
+        })
+    
+        alert (mensaje)
+    }
 }
